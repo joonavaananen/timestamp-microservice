34f47e591d0bec2545e269bf6f5d3f89dba47420
Glitch (cottony-agustinia) <none>
2020-03-22 18:18:28 +0000

    🐿🍅 Updated with Glitch

--- server.js
+++ server.js
@@ -14,35 +14,43 @@ app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// timestamp microservice API endpoint
-app.get('/api/timestamp/:date_string', function (req, res) {
-  let dateParam = req.params.date_string,
-      dateObj;
-  
-  if (dateParam) {
-    if (/^\d+$/.test(dateParam)) {
-      dateParam = parseInt(dateParam);
-    }
-    
-    try {
-      dateObj = new Date(dateParam);
-    }
-    catch {
-      dateObj = false;
-    }
-  }
-  else {
-    dateObj = new Date();
-  }
+app.get('/api/timestamp/', function(req, res) {
+  const date = new Date();
   
   res.json({
-    unix : dateObj ? dateObj.getTime() : null,
-    utc  : dateObj ? dateObj.toUTCString() : 'Invalid Date'
+    unix : date.getTime(),
+    utc  : date.toUTCString()
   });
+});
+
+// timestamp microservice API endpoint
+app.get('/api/timestamp/:date_string', function(req, res) {
+  const dateString = req.params.date_string;
+
+  if (/\d{5,}/.test(dateString)) {    
+    res.json({
+      unix : dateString,
+      utc  : new Date(parseInt(dateString)).toUTCString()
+    });
+  }
+
+  const dateObject = new Date(dateString);
+
+  if (dateObject.toString() === 'Invalid Date') {
+    res.json({
+      error : 'Invalid Date'
+    });
+  }
+  else {
+    res.json({
+      unix : dateObject.valueOf(),
+      utc  : dateObject.toUTCString()
+    });
+  }
 });
 
 // listen for requests :)
